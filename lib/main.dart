669c657fa4b39801afb8cec769dd7340d5b3c22f
AyushKadbe <ayushkadbe24@gmail.com>
2022-04-03 12:05:15 +0530

    ListView Commit showing Infinite list of Suggestions generated using WordPair

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random(); //Create a variable to store Words
-    return MaterialApp(
-      title: 'StartUP Namer',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome'), //APPBAR: HEADER says WELCOME
-        ),
-        body: Center(
-          //CENTER of the HOMESCREEN BODY says HELLO
-          child: Text(wordPair
-              .asPascalCase), //Display Variables in Pascal Case on Center of BOdy
-        ),
-      ),
+
+    return const MaterialApp(
+      //Inside MATERIAL App directly call RandomWords() to show name list
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
@@ -38,8 +31,42 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  //Add _suggestions list for saving words
+  //add _biggerfonts for font
+  final _suggestions = <WordPair>[];
+  final _biggerfonts = const TextStyle(fontSize: 18.0);
+
+//Add a ListView.builder widget to the build method of the _RandomWordsState class:
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: ListView.builder(
+        //List View
+        padding: const EdgeInsets.all(16.0),
+
+        //itemBuilder > called once/word suggested pairing >PLACES word to LIST TITLE
+        itemBuilder: (context, i) {
+          //i = rows -may be odd or even
+          if (i.isOdd) return const Divider();
+
+          //i ~/ 2 divides i by 2 and returns integer as result > calcs actual no of WORD PAIRINGS
+          //like 1,2,3,4,5 words pairing become 1,1, 2,2,3,3
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          //LISTTITLE shows the SUGGESTIONS of WordPairing in LIST with custom fonts & case
+          return ListTile(
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerfonts,
+            ),
+          );
+        },
+      ),
+    );
   }
 }
